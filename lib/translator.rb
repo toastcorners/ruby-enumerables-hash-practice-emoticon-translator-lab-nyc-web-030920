02d072203623119d1e81784c0b3a15ee9debf534
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 05:28:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,7 +15,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   # code goes here, translates eng to jap
-  emoji_hash = load_library(path)
+  emoji_hash = load_library(file_path)
   trnslt = emoji_hash["get_emoticon"][emoticon]
   trnslt 
 end
