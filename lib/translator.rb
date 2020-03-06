2ffbe76df79b0b9c99a55a66cd22192b2eea8a3d
Melissa Wise <wise.melissa19@gmail.com>
2020-03-06 05:07:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,21 +5,7 @@ def load_library(file_path)
 # code goes here, loads emoticons.yml file
 emojis = YAML.load_file('./lib/emoticons.yml')
   emojis.inspect
-  emojis = {"get_meaning" =>{
-    :☜(⌒▽⌒)☞ => "angel",
-    [ヽ(ｏ`皿′ｏ)ﾉ] => "angry",
-    [(ΘεΘ;)] => "bored",
-    [(゜.゜)] => "confused",
-    [(#^.^#)] => "embarrassed",
-    [>゜))))彡] => "fish",
-    [(^0_0^)] => "glasses",
-    [（￣ー￣）] => "grinning",
-    [（＾ｖ＾）] => "happy",
-    [(*^3^)/~☆] => "kiss",
-    [(Ｔ ▽ Ｔ)] => "sad",
-    [o_O] => "surprised",
-    [(^_-)] => "wink"
-  }, "get_emoticon" => {}}
+  emojis = {"get_meaning" =>{}, "get_emoticon" => {}}
 end
 
 def get_japanese_emoticon
